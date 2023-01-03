135f30e5da4f71108d608b021351c08fbc1e99be
QuisEgoSum <subbotin.evdokim@gmail.com>
2023-01-03 12:26:58 +0300

    feat: Add retry for network errors

--- src/app/task/TaskExecutor.ts
+++ src/app/task/TaskExecutor.ts
@@ -29,41 +29,48 @@ export class TaskExecutor {
 
   async execute(task: ITask): Promise<void> {
     this.logger.info(`Execute task ${task.number}`)
-    let status: number | string
-    try {
-      const response = await axios.get(task.url)
-      status = response.status
-    } catch (error) {
-      if (error instanceof AxiosError) {
-        if (error.response) {
-          status = error.response.status
+    for (let i = 0; i < 5; i++) {
+      let status: number | string
+      try {
+        const response = await axios.get(task.url)
+        status = response.status
+      } catch (error) {
+        if (error instanceof AxiosError) {
+          if (error.response) {
+            status = error.response.status
+          } else {
+            status = error.status || error.code || 'UNKNOWN'
+            if (i !== 4) {
+              logger.warn(`Failed request for task ${task.number} with code ${status}`)
+              continue
+            }
+          }
+        } else if (error instanceof Error) {
+          await this.sendFailedResult(
+            task,
+            `${task.url} провалена с ошибкой ${(error.message)}\n\nЗадача ${task.number}`
+          )
+          this.logger.child({task: task.number}).error(error)
+          return
         } else {
-          status = error.status || error.code || 'UNKNOWN'
+          await this.sendFailedResult(
+            task,
+            `${task.url} провалена с неизвестной ошибкой\n\nЗадача ${task.number}`
+          )
+          logger.child({task: task.number}).error(error)
+          return
         }
-      } else if (error instanceof Error) {
-        await this.sendFailedResult(
-          task,
-          `${task.url} провалена с ошибкой ${(error.message)}\n\nЗадача ${task.number}`
-        )
-        this.logger.child({task: task.number}).error(error)
-        return
-      } else {
-        await this.sendFailedResult(
-          task,
-          `${task.url} провалена с неизвестной ошибкой\n\nЗадача ${task.number}`
-        )
-        logger.child({task: task.number}).error(error)
-        return
       }
+      const executeAt = Date.now()
+      if (status !== task.status) {
+        await this.sendFailedResult(
+          task,
+          `${task.url} статус код ответа: ${status}\n\nЗадача ${task.number}`
+        )
+        this.logger.info(`Failed execute task ${task.number}`)
+      }
+      await this.repository.setExecuteInfo(task.number, executeAt, status)
+      return
     }
-    const executeAt = Date.now()
-    if (status !== task.status) {
-      await this.sendFailedResult(
-        task,
-        `${task.url} статус код ответа: ${status}\n\nЗадача ${task.number}`
-      )
-      this.logger.info(`Failed execute task ${task.number}`)
-    }
-    await this.repository.setExecuteInfo(task.number, executeAt, status)
   }
 }
