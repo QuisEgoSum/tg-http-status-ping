63dab46450344cf05780fa826688f245821ea111
QuisEgoSum <subbotin.evdokim@gmail.com>
2023-01-03 12:33:29 +0300

    feat: Create state to prevent parallel execution of a single task

--- src/app/task/TaskExecutor.ts
+++ src/app/task/TaskExecutor.ts
@@ -7,6 +7,7 @@ import {TaskRepository} from '@app/task/TaskRepository'
 
 export class TaskExecutor {
   private readonly logger = logger.child({label: 'TaskExecutor'})
+  private readonly state: Set<number> = new Set()
   constructor(
     private readonly telegram: Telegram,
     private readonly repository: TaskRepository
@@ -28,49 +29,58 @@ export class TaskExecutor {
   }
 
   async execute(task: ITask): Promise<void> {
-    this.logger.info(`Execute task ${task.number}`)
-    for (let i = 0; i < 5; i++) {
-      let status: number | string
-      try {
-        const response = await axios.get(task.url)
-        status = response.status
-      } catch (error) {
-        if (error instanceof AxiosError) {
-          if (error.response) {
-            status = error.response.status
-          } else {
-            status = error.status || error.code || 'UNKNOWN'
-            if (i !== 4) {
-              logger.warn(`Failed request for task ${task.number} with code ${status}`)
-              continue
-            }
-          }
-        } else if (error instanceof Error) {
-          await this.sendFailedResult(
-            task,
-            `${task.url} провалена с ошибкой ${(error.message)}\n\nЗадача ${task.number}`
-          )
-          this.logger.child({task: task.number}).error(error)
-          return
-        } else {
-          await this.sendFailedResult(
-            task,
-            `${task.url} провалена с неизвестной ошибкой\n\nЗадача ${task.number}`
-          )
-          logger.child({task: task.number}).error(error)
-          return
-        }
-      }
-      const executeAt = Date.now()
-      if (status !== task.status) {
-        await this.sendFailedResult(
-          task,
-          `${task.url} статус код ответа: ${status}\n\nЗадача ${task.number}`
-        )
-        this.logger.info(`Failed execute task ${task.number}`)
-      }
-      await this.repository.setExecuteInfo(task.number, executeAt, status)
+    if (this.state.has(task.number)) {
+      logger.child({task: task.number}).warn(`Skip execute task`)
       return
+    }
+    this.state.add(task.number)
+    try {
+      this.logger.info(`Execute task ${task.number}`)
+      for (let i = 0; i < 5; i++) {
+        let status: number | string
+        try {
+          const response = await axios.get(task.url)
+          status = response.status
+        } catch (error) {
+          if (error instanceof AxiosError) {
+            if (error.response) {
+              status = error.response.status
+            } else {
+              status = error.status || error.code || 'UNKNOWN'
+              if (i !== 4) {
+                logger.child({task: task.number}).warn(`Failed request for task with code ${status}`)
+                continue
+              }
+            }
+          } else if (error instanceof Error) {
+            await this.sendFailedResult(
+              task,
+              `${task.url} провалена с ошибкой ${(error.message)}\n\nЗадача ${task.number}`
+            )
+            this.logger.child({task: task.number}).error(error)
+            return
+          } else {
+            await this.sendFailedResult(
+              task,
+              `${task.url} провалена с неизвестной ошибкой\n\nЗадача ${task.number}`
+            )
+            logger.child({task: task.number}).error(error)
+            return
+          }
+        }
+        const executeAt = Date.now()
+        if (status !== task.status) {
+          await this.sendFailedResult(
+            task,
+            `${task.url} статус код ответа: ${status}\n\nЗадача ${task.number}`
+          )
+          this.logger.info(`Failed execute task ${task.number}`)
+        }
+        await this.repository.setExecuteInfo(task.number, executeAt, status)
+        return
+      }
+    } finally {
+      this.state.delete(task.number)
     }
   }
 }
